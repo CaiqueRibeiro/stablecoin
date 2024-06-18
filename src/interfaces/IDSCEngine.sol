7212fd258f3160f5001179bfc188daa326fb6521
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-06-18 19:50:52 -0300

    feature: install openzeppelin and create coins

---
+++ src/interfaces/IDSCEngine.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+// @title An interface to define DSC engine methods to be implemented
+// @notice A contract that act as an engine must implement this interface
+pragma solidity 0.8.24;
+
+interface IDSCEngine {
+    function depositCollateralAndMintDsc() external;
+
+    function redeemCollateralForDsc() external;
+
+    function burnDsc() external;
+
+    function liquidate() external;
+
+    function getHealthFactor() external view;
+}
