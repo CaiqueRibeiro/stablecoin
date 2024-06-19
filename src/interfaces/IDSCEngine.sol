5efa4c9df19177449da34825b420307f2aa825d0
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-06-19 10:52:46 -0300

    chore: create interface for engine

--- src/interfaces/IDSCEngine.sol
+++ src/interfaces/IDSCEngine.sol
@@ -7,7 +7,13 @@ pragma solidity 0.8.24;
 interface IDSCEngine {
     function depositCollateralAndMintDsc() external;
 
+    function depositCollateral() external;
+
     function redeemCollateralForDsc() external;
+
+    function redeemCollateral() external;
+
+    function mintDsc() external;
 
     function burnDsc() external;
 
